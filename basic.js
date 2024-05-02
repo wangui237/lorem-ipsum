bb4af4600f926a3752fccf2d5abbc4c40854dd51
wangui237 <priscillanjiru35@gmail.com>
2024-05-02 19:51:28 +0300

    area of a rectangle

---
+++ basic.js
@@ -0,0 +1,9 @@
+function  computeAreaOfRectangle{} < 
+let l = document.getElementById("length").nodeValue;
+let w = document.getElementById("width").nodeValue;
+
+  let area = l * w;
+  document.getElementById("output").InnerHTML = area;
+  
+  >
+   
